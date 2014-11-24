31dc2e6da7f8275b6bd6bf8d0e98c80d23a6bd39
Valak12 <Kohnechi@gmail.com>
2014-11-24 13:52:39 -0800

    Initial Commit

---
+++ City Bicycle Co./AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  City Bicycle Co.
+//
+//  Created by Vala Kohnechi on 11/24/14.
+//  Copyright (c) 2014 MVA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ City Bicycle Co./ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  City Bicycle Co.
+//
+//  Created by Vala Kohnechi on 11/24/14.
+//  Copyright (c) 2014 MVA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
