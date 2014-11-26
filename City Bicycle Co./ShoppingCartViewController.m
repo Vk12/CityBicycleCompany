cb6318a2862ab305be930ff71835f998d0a75c45
May Yang <mayyang@Mays-MacBook-Pro.local>
2014-11-25 17:22:02 -0800

    added PassKit framework

--- City Bicycle Co./ShoppingCartViewController.m
+++ City Bicycle Co./ShoppingCartViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ShoppingCartViewController.h"
+@import PassKit;
 
 @interface ShoppingCartViewController ()
 @property (strong, nonatomic) IBOutlet UIButton *buyWithApplePayButton;
@@ -22,14 +23,6 @@
 
 
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
