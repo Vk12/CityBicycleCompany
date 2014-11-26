fb77eaa51c0ad6b524c3ef7daed64a8bc7e2c9db
May Yang <mayyang@Mays-MacBook-Pro.local>
2014-11-26 13:28:24 -0800

    Stripe code

--- City Bicycle Co./ShoppingCartViewController.m
+++ City Bicycle Co./ShoppingCartViewController.m
@@ -7,7 +7,11 @@
 //
 
 #import "ShoppingCartViewController.h"
+#import "Stripe.h"
+#import "Stripe+ApplePay.h"
+
 @import PassKit;
+
 
 @interface ShoppingCartViewController ()
 @property (strong, nonatomic) IBOutlet UIButton *buyWithApplePayButton;
@@ -18,11 +22,83 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    PKPaymentRequest *request = [Stripe paymentRequestWithMerchantIdentifier:@"merchant.com.MVA-City-Bicycle-Co"];
+    
+    //Configure request here.
+    NSString *label = @"Premium Llama Food";
+    NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithString:@"10.00"];
+    request.paymentSummaryItems = @[[PKPaymentSummaryItem summaryItemWithLabel:label amount:amount]];
+    
+    if ([Stripe canSubmitPaymentRequest:request])
+    {
+        UIViewController *test;
+        
+#if DEBUG
+        
+        if (<#condition#>) {
+            <#statements#>
+        }
+        
+    }
+    
+    
+    
+    
+    
+    if ([Stripe canSubmitPaymentRequest:request])
+    {
+        
+        UIViewController *paymentController;
+        
+#if DEBUG
+
+        paymentController
+#else
+    
+#endif
+        
+        
+    }
+    
+    
+        
+        
+        
+        pa
+        
+        
+        
+        
+        
+        
+        ]
+        UIViewController *paymentController;
+        
+#if DEBUG
+        paymentController = [[STPTestPaymentAuthorizationViewController alloc]
+                             initWithPaymentRequest:];
+        paymentController.delegate = self;
+        
+#else
+        paymentController
+        
+#else
+        paymentController = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:request];
+        
+        paymentController.delegate = self;
+        
+//        paymentController.delegate = self;
+        
+#endif
+        [self presentViewController:paymentController animated:YES completion:nil];
+        
+
+        
+    }
+    else
+    {
+        // Show the user your credit card form
+    }
+
 }
-
-
-
-
-
 @end
